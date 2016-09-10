0941c2018499d96aa179c1e0a8b103e7fd10d773
Your Email Adress <raubenheimerjames@gmail.com>
2016-09-10 17:14:17 -0400

    Added all files

---
+++ app/src/main/java/cn/nekocode/camerafilter/CameraRenderer.java
@@ -0,0 +1,266 @@
+/*
+ * Copyright 2016 nekocode
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cn.nekocode.camerafilter;
+
+import android.content.Context;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+import android.opengl.GLES11Ext;
+import android.opengl.GLES20;
+import android.util.Log;
+import android.util.Pair;
+import android.util.SparseArray;
+import android.view.TextureView;
+
+import java.io.IOException;
+
+import javax.microedition.khronos.egl.EGL10;
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.egl.EGLContext;
+import javax.microedition.khronos.egl.EGLDisplay;
+import javax.microedition.khronos.egl.EGLSurface;
+
+import cn.nekocode.camerafilter.filter.AsciiArtFilter;
+import cn.nekocode.camerafilter.filter.BasicDeformFilter;
+import cn.nekocode.camerafilter.filter.BlueorangeFilter;
+import cn.nekocode.camerafilter.filter.CameraFilter;
+import cn.nekocode.camerafilter.filter.ChromaticAberrationFilter;
+import cn.nekocode.camerafilter.filter.ContrastFilter;
+import cn.nekocode.camerafilter.filter.CrackedFilter;
+import cn.nekocode.camerafilter.filter.CrosshatchFilter;
+import cn.nekocode.camerafilter.filter.EMInterferenceFilter;
+import cn.nekocode.camerafilter.filter.EdgeDetectionFilter;
+import cn.nekocode.camerafilter.filter.JFAVoronoiFilter;
+import cn.nekocode.camerafilter.filter.LegofiedFilter;
+import cn.nekocode.camerafilter.filter.LichtensteinEsqueFilter;
+import cn.nekocode.camerafilter.filter.MappingFilter;
+import cn.nekocode.camerafilter.filter.MoneyFilter;
+import cn.nekocode.camerafilter.filter.NoiseWarpFilter;
+import cn.nekocode.camerafilter.filter.OriginalFilter;
+import cn.nekocode.camerafilter.filter.PixelizeFilter;
+import cn.nekocode.camerafilter.filter.PolygonizationFilter;
+import cn.nekocode.camerafilter.filter.RefractionFilter;
+import cn.nekocode.camerafilter.filter.TileMosaicFilter;
+import cn.nekocode.camerafilter.filter.TrianglesMosaicFilter;
+
+/**
+ * Created by nekocode on 16/8/5.
+ */
+public class CameraRenderer extends Thread implements TextureView.SurfaceTextureListener {
+    private static final String TAG = "CameraRenderer";
+    private static final int EGL_OPENGL_ES2_BIT = 4;
+    private static final int EGL_CONTEXT_CLIENT_VERSION = 0x3098;
+    private static final int DRAW_INTERVAL = 1000 / 30;
+
+    private Context context;
+    private SurfaceTexture surfaceTexture;
+    private int gwidth, gheight;
+
+    private EGLDisplay eglDisplay;
+    private EGLSurface eglSurface;
+    private EGLContext eglContext;
+    private EGL10 egl10;
+
+    private Camera camera;
+    private SurfaceTexture cameraSurfaceTexture;
+    private int cameraTextureId;
+    private CameraFilter selectedFilter;
+    private SparseArray<CameraFilter> cameraFilterMap = new SparseArray<>();
+
+    public CameraRenderer(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+    }
+
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+        GLES20.glViewport(0, 0, gwidth = width, gheight = height);
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        if (camera != null) {
+            camera.stopPreview();
+            camera.release();
+        }
+        interrupt();
+        CameraFilter.release();
+
+        return true;
+    }
+
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+        if (isAlive()) {
+            interrupt();
+        }
+
+        surfaceTexture = surface;
+        GLES20.glViewport(0, 0, gwidth = width, gheight = height);
+
+        // Open camera
+        Pair<Camera.CameraInfo, Integer> backCamera = getBackCamera();
+        final int backCameraId = backCamera.second;
+        camera = Camera.open(backCameraId);
+
+        // Start rendering
+        start();
+    }
+
+    public void setSelectedFilter(int id) {
+        selectedFilter = cameraFilterMap.get(id);
+        selectedFilter.onAttach();
+    }
+
+    @Override
+    public void run() {
+        initGL(surfaceTexture);
+
+        // Setup camera filters map
+        cameraFilterMap.append(R.id.filter0, new OriginalFilter(context));
+        cameraFilterMap.append(R.id.filter1, new EdgeDetectionFilter(context));
+        cameraFilterMap.append(R.id.filter2, new PixelizeFilter(context));
+        cameraFilterMap.append(R.id.filter3, new EMInterferenceFilter(context));
+        cameraFilterMap.append(R.id.filter4, new TrianglesMosaicFilter(context));
+        cameraFilterMap.append(R.id.filter5, new LegofiedFilter(context));
+        cameraFilterMap.append(R.id.filter6, new TileMosaicFilter(context));
+        cameraFilterMap.append(R.id.filter7, new BlueorangeFilter(context));
+        cameraFilterMap.append(R.id.filter8, new ChromaticAberrationFilter(context));
+        cameraFilterMap.append(R.id.filter9, new BasicDeformFilter(context));
+        cameraFilterMap.append(R.id.filter10, new ContrastFilter(context));
+        cameraFilterMap.append(R.id.filter11, new NoiseWarpFilter(context));
+        cameraFilterMap.append(R.id.filter12, new RefractionFilter(context));
+        cameraFilterMap.append(R.id.filter13, new MappingFilter(context));
+        cameraFilterMap.append(R.id.filter14, new CrosshatchFilter(context));
+        cameraFilterMap.append(R.id.filter15, new LichtensteinEsqueFilter(context));
+        cameraFilterMap.append(R.id.filter16, new AsciiArtFilter(context));
+        cameraFilterMap.append(R.id.filter17, new MoneyFilter(context));
+        cameraFilterMap.append(R.id.filter18, new CrackedFilter(context));
+        cameraFilterMap.append(R.id.filter19, new PolygonizationFilter(context));
+        cameraFilterMap.append(R.id.filter20, new JFAVoronoiFilter(context));
+        setSelectedFilter(R.id.filter0);
+
+        // Create texture for camera preview
+        cameraTextureId = MyGLUtils.genTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES);
+        cameraSurfaceTexture = new SurfaceTexture(cameraTextureId);
+
+        // Start camera preview
+        try {
+            camera.setPreviewTexture(cameraSurfaceTexture);
+            camera.startPreview();
+        } catch (IOException ioe) {
+            // Something bad happened
+        }
+
+        // Render loop
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+
+                // Update the camera preview texture
+                synchronized (this) {
+                    cameraSurfaceTexture.updateTexImage();
+                }
+
+                // Draw camera preview
+                selectedFilter.draw(cameraTextureId, gwidth, gheight);
+
+                // Flush
+                GLES20.glFlush();
+                egl10.eglSwapBuffers(eglDisplay, eglSurface);
+
+                Thread.sleep(DRAW_INTERVAL);
+
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+
+        cameraSurfaceTexture.release();
+        GLES20.glDeleteTextures(1, new int[]{cameraTextureId}, 0);
+    }
+
+    private void initGL(SurfaceTexture texture) {
+        egl10 = (EGL10) EGLContext.getEGL();
+
+        eglDisplay = egl10.eglGetDisplay(EGL10.EGL_DEFAULT_DISPLAY);
+        if (eglDisplay == EGL10.EGL_NO_DISPLAY) {
+            throw new RuntimeException("eglGetDisplay failed " + android.opengl.GLUtils.getEGLErrorString(egl10.eglGetError()));
+        }
+
+        int[] version = new int[2];
+        if (!egl10.eglInitialize(eglDisplay, version)) {
+            throw new RuntimeException("eglInitialize failed " + android.opengl.GLUtils.getEGLErrorString(egl10.eglGetError()));
+        }
+
+        int[] configsCount = new int[1];
+        EGLConfig[] configs = new EGLConfig[1];
+        int[] configSpec = {
+                EGL10.EGL_RENDERABLE_TYPE,
+                EGL_OPENGL_ES2_BIT,
+                EGL10.EGL_RED_SIZE, 8,
+                EGL10.EGL_GREEN_SIZE, 8,
+                EGL10.EGL_BLUE_SIZE, 8,
+                EGL10.EGL_ALPHA_SIZE, 8,
+                EGL10.EGL_DEPTH_SIZE, 0,
+                EGL10.EGL_STENCIL_SIZE, 0,
+                EGL10.EGL_NONE
+        };
+
+        EGLConfig eglConfig = null;
+        if (!egl10.eglChooseConfig(eglDisplay, configSpec, configs, 1, configsCount)) {
+            throw new IllegalArgumentException("eglChooseConfig failed " + android.opengl.GLUtils.getEGLErrorString(egl10.eglGetError()));
+        } else if (configsCount[0] > 0) {
+            eglConfig = configs[0];
+        }
+        if (eglConfig == null) {
+            throw new RuntimeException("eglConfig not initialized");
+        }
+
+        int[] attrib_list = {EGL_CONTEXT_CLIENT_VERSION, 2, EGL10.EGL_NONE};
+        eglContext = egl10.eglCreateContext(eglDisplay, eglConfig, EGL10.EGL_NO_CONTEXT, attrib_list);
+        eglSurface = egl10.eglCreateWindowSurface(eglDisplay, eglConfig, texture, null);
+
+        if (eglSurface == null || eglSurface == EGL10.EGL_NO_SURFACE) {
+            int error = egl10.eglGetError();
+            if (error == EGL10.EGL_BAD_NATIVE_WINDOW) {
+                Log.e(TAG, "eglCreateWindowSurface returned EGL10.EGL_BAD_NATIVE_WINDOW");
+                return;
+            }
+            throw new RuntimeException("eglCreateWindowSurface failed " + android.opengl.GLUtils.getEGLErrorString(error));
+        }
+
+        if (!egl10.eglMakeCurrent(eglDisplay, eglSurface, eglSurface, eglContext)) {
+            throw new RuntimeException("eglMakeCurrent failed " + android.opengl.GLUtils.getEGLErrorString(egl10.eglGetError()));
+        }
+    }
+
+    private Pair<Camera.CameraInfo, Integer> getBackCamera() {
+        Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
+        final int numberOfCameras = Camera.getNumberOfCameras();
+
+        for (int i = 0; i < numberOfCameras; ++i) {
+            Camera.getCameraInfo(i, cameraInfo);
+            if (cameraInfo.facing == Camera.CameraInfo.CAMERA_FACING_BACK) {
+                return new Pair<>(cameraInfo, i);
+            }
+        }
+        return null;
+    }
+}
---
+++ app/src/main/java/cn/nekocode/camerafilter/MainActivity.java
@@ -0,0 +1,184 @@
+/*
+ * Copyright 2016 nekocode
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cn.nekocode.camerafilter;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CAMERA_PERMISSION = 101;
+    private CameraRenderer renderer;
+    private TextureView textureView;
+    private View crosshairs;
+    private int filterId = R.id.filter0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        setTitle("Original");
+
+        crosshairs= findViewById(R.id.crosshairs);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                Toast.makeText(this, "Camera access is required.", Toast.LENGTH_SHORT).show();
+
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                        REQUEST_CAMERA_PERMISSION);
+            }
+
+        } else {
+            setupCameraPreviewView();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CAMERA_PERMISSION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    setupCameraPreviewView();
+                }
+            }
+        }
+    }
+
+    void setupCameraPreviewView() {
+        renderer = new CameraRenderer(this);
+        textureView = (TextureView) findViewById(R.id.textureView);
+        assert textureView != null;
+        textureView.setSurfaceTextureListener(renderer);
+
+        // Show original frame when touch the view
+        textureView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        renderer.setSelectedFilter(R.id.filter0);
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                    case MotionEvent.ACTION_CANCEL:
+                        renderer.setSelectedFilter(filterId);
+                        break;
+                }
+                return true;
+            }
+        });
+
+        textureView.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+            @Override
+            public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+                renderer.onSurfaceTextureSizeChanged(null, v.getWidth(), v.getHeight());
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.filter, menu);
+        return true;
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        filterId = item.getItemId();
+
+        // TODO: need tidy up
+        if (filterId == R.id.capture) {
+            Toast.makeText(this,
+                    caputre() ? "The capture has been saved to your sdcard root path." :
+                            "Save failed!",
+                    Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        setTitle(item.getTitle());
+
+        if (renderer != null)
+            renderer.setSelectedFilter(filterId);
+
+        return true;
+    }
+
+    private boolean caputre() {
+        String mPath = genSaveFileName(getTitle().toString() + "_", ".png");
+        File imageFile = new File(mPath);
+        if (imageFile.exists()) {
+            imageFile.delete();
+        }
+
+        // create bitmap screen capture
+        Bitmap bitmap = textureView.getBitmap();
+        OutputStream outputStream = null;
+
+        try {
+            outputStream = new FileOutputStream(imageFile);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 90, outputStream);
+            outputStream.flush();
+            outputStream.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    private String genSaveFileName(String prefix, String suffix) {
+        Date date = new Date();
+        SimpleDateFormat dateformat1 = new SimpleDateFormat("yyyyMMdd_hhmmss");
+        String timeString = dateformat1.format(date);
+        String externalPath = Environment.getExternalStorageDirectory().toString();
+        return externalPath + "/" + prefix + timeString + suffix;
+    }
+
+
+}
+
